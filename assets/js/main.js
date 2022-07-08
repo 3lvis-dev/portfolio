a0566263434442ca2bcf41c7b3ba4025e06806ff
3lvis-dev <ea.espinoza.f@gmail.com>
2022-07-08 18:43:49 -0400

    add menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello, porfolio");
+const iconBurger = document.querySelector("#icon_burger");
+const menu = document.querySelector("#menu");
+
+iconBurger.addEventListener("click", () => {
+    menu.classList.toggle("show_menu");
+});
